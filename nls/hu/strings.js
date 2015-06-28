89edc4f413a5eaea0b1e9febc448a217a2f1fcd6
Tamás Horváth <htomy92@gmail.com>
2015-06-28 23:20:03 +0200

    Fixing translation files - translate update
    https://github.com/Equals182/eqFTP/commit/0dd2f69f4644a3ed165fff22b20f458b64b75f4d

--- nls/hu/strings.js
+++ nls/hu/strings.js
@@ -17,7 +17,7 @@ define({
     "ERR_DIAG_SERVCANTREACH_CONTENT" : "Nem tudom elérni a szervert.<br>Lehetséges, hogy a tűzfal akadályozza ezt meg.",
     "ERR_DIAG_AUTHORIZEERR_TITLE" : "Hibás hitelesítő adatok",
     "ERR_DIAG_AUTHORIZEERR_CONTENT" : "Hibás felhasználónév vagy jelszó.<br>Kérlek ellenőrizd őket.",
-    "ERR_DIAG_NOSERVERFOUND_TITLE" : "Nincsenek kapcsolatok-Can't find connection",
+    "ERR_DIAG_NOSERVERFOUND_TITLE" : "Nem található kapcsolat",
     "ERR_DIAG_NOSERVERFOUND_CONTENT" : "Nincs beállítva kapcsolat amit használni tudnék.<br>Kapcsolódj egy szerverhez vagy adj meg egy helyi projekt könyvtárat kapcsolatként a Beállítások menüpontban.",
     "ERR_DIAG_ECONNRESET_TITLE" : "A szerver visszautasította a kapcsolódást",
     "ERR_DIAG_ECONNRESET_CONTENT" : "Nem tudok kapcsolódni a szerverhez, mert az visszautasította a kérést.<br>Tipp: Próbáld újraindítani a programot.",
@@ -25,23 +25,23 @@ define({
     "ERR_DIAG_ECONNABORTED_CONTENT" : "Kapcsolódás megszakítva. Újrapróbálkozás...",
     "ERR_DIAG_UNIVERSAL_TITLE" : "HIBA!",
     "ERR_DIAG_UNIVERSAL_CONTENT" : "Sajnos általános hiba történt. :(",
-    "ERR_DIAG_NORSAKEYFOUND" : "There's no RSA key found in: ", //NEW A path to RSA file will be appended after colon.
+    "ERR_DIAG_NORSAKEYFOUND" : "Az RSA kulcs nem található itt: ",
 
     "NOT_DIAG_CONNECTED" : "Kapcsolódva.",
     "NOT_DIAG_DISCONNECTED" : "Kapcsolat bontva.",
-    "NOT_DIAG_FILESDIFFERENT" : "There's a difference between local and remote files. Click here for more actions.", //NEW
+    "NOT_DIAG_FILESDIFFERENT" : "A helyi és a távoli fájlok nem egyeznek meg. További információkért kattints ide.",
 
     "PASSWDWIND_TITLE" : "Kérlek írd be az FTP jelszavad",
     "PASSWDWIND_CONTENT" : "FTP jelszó:",
 
-    "CHECKDIFF_TITLE" : "There's difference between files", //NEW
-    "CHECKDIFF_CONTENT" : "Remote and local copies of file are different. Please choose an action you would like to do.", //NEW
-    "CHECKDIFF_BUTTON_COMPARE" : "Compare files", //NEW
-    "CHECKDIFF_BUTTON_SHOWCHANGES" : "Show changes", //NEW
-    "CHECKDIFF_BUTTON_KEEPLOCAL" : "Keep local", //NEW
-    "CHECKDIFF_BUTTON_KEEPLOCALALL" : "Keep local copies for current queue", //NEW
-    "CHECKDIFF_BUTTON_GETREMOTE" : "Get remote", //NEW
-    "CHECKDIFF_BUTTON_GETREMOTEALL" : "Get remote copies for current queue", //NEW
+    "CHECKDIFF_TITLE" : "Fájlok különböznek",
+    "CHECKDIFF_CONTENT" : "Távoli és helyi fájlok másolatai nem egyeznek meg. Kérlek válassz másik lehetőséget.",
+    "CHECKDIFF_BUTTON_COMPARE" : "Fájlok összehasonlítása",
+    "CHECKDIFF_BUTTON_SHOWCHANGES" : "Változások mutatása",
+    "CHECKDIFF_BUTTON_KEEPLOCAL" : "Maradjon helyi",
+    "CHECKDIFF_BUTTON_KEEPLOCALALL" : "Tartsd meg a helyi másolatokat az aktuális várósorban",
+    "CHECKDIFF_BUTTON_GETREMOTE" : "Távirányítás megszerzése",
+    "CHECKDIFF_BUTTON_GETREMOTEALL" : "Távoli másolatok a jelenlegi sorba",
 
     "SETTINGSWIND_TITLE" : "eqFTP Kapcsolatkezelő",
     "SETTINGSWIND_ERR_BLANKS" : "Hopp! Úgy tűnik valami nincs rendben. Kérlek töltsd ki a mezőket és próbáld újra!",
@@ -50,20 +50,20 @@ define({
     "SETTINGSWIND_NOTIF_DONE" : "Grat! Minden mentve! :)",
     "SETTINGSWIND_SAVEDCONN" : "Mentett kapcsolatok",
     "SETTINGSWIND_DELETECONN_HOVER" : "Kapcsolat törlése",
-    "SETTINGSWIND_COPYCONN_HOVER" : "Copy This Connection", //NEW
-    "SETTINGSWIND_ERROR_DELETE_CURCONNSERV" : "You can't delete connection you're using now", //NEW
+    "SETTINGSWIND_COPYCONN_HOVER" : "Kapcsolat másolása",
+    "SETTINGSWIND_ERROR_DELETE_CURCONNSERV" : "Nem törölhető, mert jelenleg használatban van.",
     "SETTINGSWIND_ADDCONN_HOVER" : "Új kapcsolat",
     "SETTINGSWIND_ADDCONN_STRING" : "Új kapcsolat létrehozása...",
     "SETTINGSWIND_DELETECONNCONF_1" : "Kérlek erősítsd meg a ",
     "SETTINGSWIND_DELETECONNCONF_2" : " kapcsolat törlését.",
     "SETTINGSWIND_OPENGLOBSET" : "Globális FTP beállítások megnyitása...",
     "SETTINGSWIND_NOTHINGYETMSG" : "Válaszd ki a módosítani kívánt kapcsolatot vagy az oldalsávon hozz létre egyet.",
-    "SETTINGSWIND_GLOB_FOLDERFORPROJ" : "Folder For Projects:", //NEW
-    "SETTINGSWIND_GLOB_FOLDERFORPROJ_DIAGTITLE" : "Directory for Projects with Downloaded Files:", //NEW
-    "SETTINGSWIND_GLOB_FOLDERFORSET" : "Folder For Settings:", //NEW
-    "SETTINGSWIND_GLOB_FOLDERFORSET_DIAGTITLE" : "Directory for Settings File", //NEW
+    "SETTINGSWIND_GLOB_FOLDERFORPROJ" : "Projektek mappája:",
+    "SETTINGSWIND_GLOB_FOLDERFORPROJ_DIAGTITLE" : "Mappa a projektek számára a letöltött fájlokkal:", //NEW
+    "SETTINGSWIND_GLOB_FOLDERFORSET" : "Beállítások mappája:",
+    "SETTINGSWIND_GLOB_FOLDERFORSET_DIAGTITLE" : "Beállítási fájl könyvtárra",
     "SETTINGSWIND_GLOB_DONTOPENPROJECTS" : "Ne nyisd meg a projekteket a letöltés után",
-    "SETTINGSWIND_GLOB_MASTERPASSWORD" : "Master Password", //NEW
+    "SETTINGSWIND_GLOB_MASTERPASSWORD" : "Mester jelszó",
     "SETTINGSWIND_GLOB_CONNECTIONNAME_TITLE" : "Kapcsolat neve:",
     "SETTINGSWIND_GLOB_CONNECTIONNAME_FIELD" : "Írd be a kapcsolat nevét",
     "SETTINGSWIND_GLOB_SERVER_TITLE" : "Szerver:",
@@ -72,22 +72,22 @@ define({
     "SETTINGSWIND_GLOB_USERNAME_FIELD" : "FTP felhasználónév",
     "SETTINGSWIND_GLOB_PASSWORD_TITLE" : "Jelszó:",
     "SETTINGSWIND_GLOB_PASSWORD_FIELD" : "FTP jelszó",
-    "SETTINGSWIND_GLOB_RSA_TITLE" : "RSA key", //NEW
-    "SETTINGSWIND_GLOB_RSA_FIELD" : "Click to select RSA key", //NEW
-    "SETTINGSWIND_GLOB_FOLDERFORRSA_DIAGTITLE" : "Choose path to RSA key", //NEW
+    "SETTINGSWIND_GLOB_RSA_TITLE" : "RSA kulcs",
+    "SETTINGSWIND_GLOB_RSA_FIELD" : "Kattints ide az RSA kulcsért",
+    "SETTINGSWIND_GLOB_FOLDERFORRSA_DIAGTITLE" : "Válassz elérési útvonalat az RSA kulcshoz.",
     "SETTINGSWIND_GLOB_REMOTEPATH_TITLE" : "Távoli útvonal:",
-    "SETTINGSWIND_GLOB_REMOTEPATH_FIELD" : "Type in the remote path to the project root folder", //NEW
+    "SETTINGSWIND_GLOB_REMOTEPATH_FIELD" : "Írja be a távoli elérési utat a projekt gyökérmappához",
     "SETTINGSWIND_GLOB_LOCALPATH_TITLE" : "Helyi útvonal:",
     "SETTINGSWIND_GLOB_LOCALPATH_FIELD" : "Ha üres, az alapértelmezett könyvtárban lesz létrehozva",
     "SETTINGSWIND_GLOB_UPLOADONSAVE" : "Mentéskor feltöltés",
-    "SETTINGSWIND_GLOB_UPLOADONSAVEPAUSED" : "Add to paused queue.", //NEW
+    "SETTINGSWIND_GLOB_UPLOADONSAVEPAUSED" : "Hozzáadás a szüneteltetett várósorhoz.",
     "SETTINGSWIND_GLOB_EVENDISCONN" : "Bontott kapcsolat esetén is",
-    "SETTINGSWIND_GLOB_EVENDISCONN_EXPLAIN" : "This option automatically connects to a server when a saved file is opened from the server or a saved file is within the current project and this project was created by this ftp extension. Oh, I really hope I explained it well...",
+    "SETTINGSWIND_GLOB_EVENDISCONN_EXPLAIN" : "Ez az opció automatikusan kapcsolódik egy szerverhez amikor a mentett fájlt megnyitják a szerverről, vagy a mentett fájl a jelenlegi projekt része, amely projekt ezzel az ftp bővítménnyel lett létrehozva. Remélem jól elmagyaráztam.",
     "SETTINGSWIND_GLOB_LOCALPATH_FILLWPROJECT_TITLE" : "Jelenlegi projekt útvonal beszúrása",
-    "SETTINGSWIND_GLOB_FRM_TITLE" : "Choose folder retrieving method:", //NEW
-    "SETTINGSWIND_GLOB_FRM_LIST" : "Use LIST command", //NEW
-    "SETTINGSWIND_GLOB_FRM_MLSD" : "Use MLSD command", //NEW
-    "SETTINGSWIND_GLOB_KEEPALIVE" : "Keep alive:", // Keep Alive is feature when client sends empty packages to server to prevet disconnection.
+    "SETTINGSWIND_GLOB_FRM_TITLE" : "Mappa helyreállítási módszer kiválasztás:",
+    "SETTINGSWIND_GLOB_FRM_LIST" : "Használd a LIST parancsot",
+    "SETTINGSWIND_GLOB_FRM_MLSD" : "Használd az MLSD parancsot",
+    "SETTINGSWIND_GLOB_KEEPALIVE" : "Kapcsolat életben tartása:",
     "SETTINGSWIND_GLOB_KEEPALIVE_DESC" : "Az érték másodpercben. Ajánlott érték: 10. 0 esetén kikapcsolva.",
     "SETTINGSWIND_GLOB_DEBUG" : "Debug:",
     "SETTINGSWIND_GLOB_TIMEFORMAT" : "Dátum formátum:",
@@ -97,50 +97,50 @@ define({
     "SETTINGSWIND_GLOB_AUTOCLEAR" : "Várólista automatikus ürítése-Auto-clear Queue",
     "SETTINGSWIND_GLOB_NOTIFICATIONS" : "Értesítések",
     "SETTINGSWIND_GLOB_TIMEOFFSET" : "Időeltérés:",
-    "SETTINGSWIND_GLOB_TIMEOFFSET_DESC" : "Ezzel a beállítással korrigálni tudod a 'Modosítva' oszlop tartalmát",
-    "SETTINGSWIND_GLOB_CONNECTIONTAB" : "Connection", //NEW
-    "SETTINGSWIND_GLOB_AUTOMATIZATIONTAB" : "Automatization", //NEW
-    "SETTINGSWIND_GLOB_ADVANCEDTAB" : "Advanced", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SET_TITLE" : "Set automatization:", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC" : "Sync", //NEW
-    "SETTINGSWIND_GLOB_AUTO_CLASSIC" : "Classic", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_SELECT_TITLE" : "Select what to sync", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_FILECREATION" : "File creation", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_FOLDERCREATION" : "Folder creation", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_FILEUPLOAD" : "File update", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_RENAMING" : "Renaming", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_MOVING" : "Moving", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_DELETING" : "Deleting", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_EXTRASETTINGS_TITLE" : "Extra settings", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_CHECKDIFF" : "Check for differences", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_CHECKDIFF_EXPLAIN" : "When you open local or remote file", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_IGNORELIST_TITLE" : "Ignore list", //NEW
-    "SETTINGSWIND_GLOB_AUTO_SYNC_IGNORELIST_EXPLAIN" : "Same syntax as in .gitignore", //NEW
-    "SETTINGSWIND_GLOB_SYNCLOCPROJWCONN" : "Open related Local project after switching connection and vice versa", //NEW
-    "SETTINGS_NOTIF_RELOADED" : "Settings were successfully reloaded.", //NEW
-    "SETTINGS_ERROR_RELOADED" : "Can't reload settings.", //NEW
-    "SETTINGSWIND_GLOB_SCE_TITLE" : "Use SSH commands with SFTP", //NEW
+    "SETTINGSWIND_GLOB_TIMEOFFSET_DESC" : "Ezzel a beállítással korrigálni tudod a 'Módosítva' oszlop tartalmát",
+    "SETTINGSWIND_GLOB_CONNECTIONTAB" : "Kapcsolat",
+    "SETTINGSWIND_GLOB_AUTOMATIZATIONTAB" : "Automatizálás",
+    "SETTINGSWIND_GLOB_ADVANCEDTAB" : "Haladó",
+    "SETTINGSWIND_GLOB_AUTO_SET_TITLE" : "Automatizálás beállítása:",
+    "SETTINGSWIND_GLOB_AUTO_SYNC" : "Szinkronizálás",
+    "SETTINGSWIND_GLOB_AUTO_CLASSIC" : "Klasszikus",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_SELECT_TITLE" : "Válaszd ki, hogy mit szinkronizáljak",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_FILECREATION" : "Fájl létrehozása",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_FOLDERCREATION" : "Mappa létrehozása",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_FILEUPLOAD" : "Fájl frissítés",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_RENAMING" : "Átnevezés",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_MOVING" : "Áthelyezés",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_DELETING" : "Törlés",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_EXTRASETTINGS_TITLE" : "Extra beállítások",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_CHECKDIFF" : "Különbségek ellenőrzése",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_CHECKDIFF_EXPLAIN" : "Amikor megnyitod a helyi vagy a távoli fájlt",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_IGNORELIST_TITLE" : "Lista mellőzése",
+    "SETTINGSWIND_GLOB_AUTO_SYNC_IGNORELIST_EXPLAIN" : "Ugyan az a szintaxis, mint a .gitignore",
+    "SETTINGSWIND_GLOB_SYNCLOCPROJWCONN" : "Kapcsolódó Helyi projektek megnyitása kapcsolat váltás után, és fordítva",
+    "SETTINGS_NOTIF_RELOADED" : "A beállítások sikeresen újratölve.",
+    "SETTINGS_ERROR_RELOADED" : "Sikertelen a beállítások betöltése.",
+    "SETTINGSWIND_GLOB_SCE_TITLE" : "Használd az SSH parancsokat az SFTP-vel.",
 
-    "QUEUE_TITLE" : "eqFTP városor",
+    "QUEUE_TITLE" : "eqFTP várósor",
     "QUEUE_HEADER_NAME" : "Név",
     "QUEUE_HEADER_PATH" : "Útvonal",
     "QUEUE_HEADER_FROM" : "Forrás",
     "QUEUE_HEADER_TO" : "Cél",
     "QUEUE_HEADER_STATUS" : "Állapot",
-    "QUEUE_CLEARQ" : "Városor ürítése",
-    "QUEUE_STARTQ" : "Start Queue", //NEW
-    "QUEUE_PAUSEQ" : "Pause Queue", //NEW
-    "QUEUE_RESTARTQ" : "Restart Failed", //NEW
-    "QUEUE_CONTEXTM_STARTT" : "Start", //NEW
-    "QUEUE_CONTEXTM_PAUSET" : "Pause", //NEW
-    "QUEUE_CONTEXTM_REMOVET" : "Remove", //NEW
-    "QUEUE_CONTEXTM_RESTARTT" : "Restart", //NEW
-    "QUEUE_TASK_STATUS_WAITING" : "Waiting", //NEW
-    "QUEUE_TASK_STATUS_SUCCESS" : "Done", //NEW
-    "QUEUE_TASK_STATUS_FAIL" : "Failed", //NEW
-    "QUEUE_TASK_STATUS_PAUSE" : "Paused", //NEW
-    "QUEUE_TASK_STATUS_STARTED" : "Started", //NEW
-    "QUEUE_TASK_STATUS_DELETED" : "Deleted", //NEW
+    "QUEUE_CLEARQ" : "Várósor ürítése",
+    "QUEUE_STARTQ" : "Várósor inditása",
+    "QUEUE_PAUSEQ" : "Várósor szüneltetése",
+    "QUEUE_RESTARTQ" : "Újrainditás sikertelen",
+    "QUEUE_CONTEXTM_STARTT" : "Inditás",
+    "QUEUE_CONTEXTM_PAUSET" : "Szüneteltetés",
+    "QUEUE_CONTEXTM_REMOVET" : "Eltávolítás",
+    "QUEUE_CONTEXTM_RESTARTT" : "Újrainditás",
+    "QUEUE_TASK_STATUS_WAITING" : "Várakozás",
+    "QUEUE_TASK_STATUS_SUCCESS" : "Siker",
+    "QUEUE_TASK_STATUS_FAIL" : "Sikertelen",
+    "QUEUE_TASK_STATUS_PAUSE" : "Szüneteltetve",
+    "QUEUE_TASK_STATUS_STARTED" : "Elinditva",
+    "QUEUE_TASK_STATUS_DELETED" : "Törölve",
     "QUEUE_DONT_ADD_COMPLETED" : "Elkészült feladatok automatikus ürítése",
     
     "ERR_FILE_ACCESSDENIED" : "Hozzáférés megtagadva. Ellenőrizd a fájl/könyvtár jogosultságait.",
@@ -149,42 +149,42 @@ define({
     "ERR_FILE_SERVCANTREACH" : "Nem tudom elérni a szervert. Tipp: Ellenőrízd a tűzfalbeállításokat.",
     "ERR_FILE_FILESIZE0" : "Nem tudom letölteni a fájlt. A fájl mérete: 0B.",
     "ERR_FILE_DOWNLOAD" : "Nem tudom letölteni a fájlt.",
-    "ERR_FILE_UPLOAD" : "Can't upload file.", //NEW
+    "ERR_FILE_UPLOAD" : "Sikertelen a fájl feltöltése.",
     "ERR_FILE_DOESNTEXIST" : "Fájl nem létezik.",
-    "ERR_FILE_CANTRENAME" : "Can't rename file: ", //NEW
-    "ERR_FILE_CANTDELETE" : "Can't delete file: ", //NEW
-    "ERR_FILE_CANTCREATEDIR" : "Can't create folder: ", //NEW
-    "ERR_FILE_CANTDELETEDIR" : "Can't delete folder: ", //NEW
-    "ERR_FILE_CANTCREATEFILE" : "Can't create file: ", //NEW
+    "ERR_FILE_CANTRENAME" : "Sikertelen a fájlt átnevezése: ",
+    "ERR_FILE_CANTDELETE" : "Sikertelen a fájlt törlése: ",
+    "ERR_FILE_CANTCREATEDIR" : "Sikertelen a mappa létrehozása: ",
+    "ERR_FILE_CANTDELETEDIR" : "Sikertelen a mappa törlése: ",
+    "ERR_FILE_CANTCREATEFILE" : "Sikertelen a fájl létrehozása: ",
     "ERR_FOLDER_OPEN" : "Hiba történt a könytár projektként való megnyitásakor.",
 
     "CONTEXTM_DOWNLOAD" : "Letöltés",
     "CONTEXTM_UPLOAD" : "Feltöltés",
-    "CONTEXTM_ADDQUEUE" : "Hozzáadás a városorhoz",
+    "CONTEXTM_ADDQUEUE" : "Hozzáadás a várósorhoz",
     "CONTEXTM_OPEN" : "Megnyitás",
     "CONTEXTM_DELETE" : "Törlés",
     "CONTEXTM_RENAME" : "Átnevezés",
     "CONTEXTM_REDOWNLOAD" : "Letöltés a szerverről",
-    "CONTEXTM_CREATEFILE" : "Create file", //NEW
-    "CONTEXTM_CREATEFOLDER" : "Create folder", //NEW
+    "CONTEXTM_CREATEFILE" : "Új fájl",
+    "CONTEXTM_CREATEFOLDER" : "Új mappa",
 
-    "OTHER_SELECT_SERVER_DROPDOWN" : "Válaszd ki a távoli szervert a kapcsolódáshoz...",
+    "OTHER_SELECT_SERVER_DROPDOWN" : "Válaszd ki a kiszolgálót...", //Too long hungarian translation
     "OTHER_ERROR" : "Hiba!",
     "OTHER_PAUSED" : "Szüneteltetve",
     "OTHER_COMPLETED" : "Elkészült",
     "OTHER_CANCELLED" : "Megszakítva",
     "OTHER_WAITING" : "Várakozik...",
-    "OTHER_YES" : "Yes", //NEW
-    "OTHER_NO" : "No", //NEW
+    "OTHER_YES" : "Igen",
+    "OTHER_NO" : "Nem",
     "OTHER_OK" : "Ok",
-    "OTHER_OFF" : "Off", //NEW
+    "OTHER_OFF" : "Ki",
     "OTHER_CANCEL" : "Mégsem",
     "OTHER_APPLY" : "Alkalmaz",
-    "OTHER_SAVE" : "Save", //NEW
+    "OTHER_SAVE" : "Mentés",
     "OTHER_CLOSE" : "Bezárás",
     "OTHER_DELETE" : "Törlés",
     "OTHER_CONFIRM_DELETE" : "Biztosan törölni szeretnéd?",
-    "OTHER_CONFIRM_SETTINGSCLOSE" : "There are some unsaved changes that will be lost if you click OK.<br>Do you want to proceed?", //NEW
+    "OTHER_CONFIRM_SETTINGSCLOSE" : "Van pár nem mentett változás, ami el fog veszni, ha az OK gombra kattintasz.<br>Szeretnéd folytatni?",
     "OTHER_ERROR_EVENDISCONN" : "Nem tudom feltölteni a fájlt. A 'Bontott kapcsolat esetén is' beállítás ki van kapcsolva.",
-    "OTHER_ERROR_CANTREADSETTINGS" : "Can't read settings file. You probably mistyped your master password." //NEW
+    "OTHER_ERROR_CANTREADSETTINGS" : "Nem olvasható a beállítások fájl. Valószínűleg elgépelted a mester jelszót."
 });
